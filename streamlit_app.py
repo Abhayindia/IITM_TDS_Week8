b19a2909f2379fe1328ec0b390f750377f7eebc1
Abhay Chaudhary <53985942+Abhayindia@users.noreply.github.com>
2023-04-19 15:43:37 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# TDS8
+import streamlit as st
+
+def find_largest(num1, num2, num3):
+    """
+    Find The Largest
+    """
+   
+    
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+def main():
+    """
+    This function defines the Streamlit app UI and logic.
+    """
+    st.title("Find the Largest Number ")
+    st.write('Enter three numbers and find the largest among them:')
+    num1 = st.number_input("Enter the first  number", value=0, step=1)
+    num2 = st.number_input("Enter the second  number", value=0, step=1)
+    num3 = st.number_input("Enter the third  number", value=0, step=1)
+
+    if st.button("Find the largest"):
+        largest_num = find_largest(num1, num2, num3)
+        st.success(f"The largest number is {largest_num}")
+
+if __name__ == "__main__":
+    main()
